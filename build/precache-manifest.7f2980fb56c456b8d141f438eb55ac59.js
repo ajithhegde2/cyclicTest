5c3c83aab45c37a0e41445f4907b4fb2691206bb
ajithhegde2 <ajith.hegde2@gmail.com>
2022-11-25 22:11:31 +0530

    updated

---
+++ build/precache-manifest.7f2980fb56c456b8d141f438eb55ac59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0166cf74e24a3716061de8d1ecb8a17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e657712408c4bc9e9dcc",
+    "url": "/static/css/main.0dd7ec39.chunk.css"
+  },
+  {
+    "revision": "c142cb9aafb61772162e",
+    "url": "/static/js/2.21bcedf7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.21bcedf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e657712408c4bc9e9dcc",
+    "url": "/static/js/main.e0840b5a.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
